0d9dc1822d77f17a8a432b42a8f3f672be0b9b81
Elias SACI <esaci@student.42.fr>
2022-02-16 18:05:27 +0100

    auto inception passe

---
+++ objects/main.d
@@ -0,0 +1,5 @@
+objects/main.o: main.cpp \
+  /goinfre/esaci/Container/testermazoise/test_utils.hpp \
+  include/vector.hpp include/is_class.hpp include/iterator.hpp \
+  include/iterator_operators.hpp include/iterator_random.hpp \
+  include/reverse_iterator.hpp
