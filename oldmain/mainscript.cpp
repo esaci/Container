d209192ea6100d6227720e3cba0bea0141f6ecb5
Elias SACI <esaci@student.42.fr>
2022-02-28 04:16:33 +0100

    auto

--- oldmain/mainscript.cpp
+++ oldmain/mainscript.cpp
@@ -10,25 +10,13 @@
 
 
 int main( void ){
-	if(VCTR)
-	{
 		std::cout << "TEST VECTOR" << std::endl;
-		lol::vector<int> vec;
+		ft::map<int, int> mp;
 
 		for (int i = 0; i < 100000000; ++i)
-			vec.push_back(i);
-		while(vec.empty() == false)
-			vec.pop_back();
-	}
-	if (mp)
-	{
-		std::cout << "TEST MAP" << std::endl;
-		lol::map<int, int> maparg;
-
-		for (int i = 0; i < 100; ++i)
-			maparg.insert(ft::pair<int, int>(i, i));
-		while(maparg.empty() == false)
-			maparg.erase(--(maparg.end()));
-	}
+			mp.insert(ft::pair<int, int>(i, i));
+		ft::map<int, int>::iterator it = mp.begin();
+		while(it != mp.end())
+			mp.erase(it);
 	return (0);
 }
